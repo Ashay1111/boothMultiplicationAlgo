43e4d433ab8597713843ecb070f0793b7fe3e7f3
Ashay1111 <ashayjpatel@gmail.com>
2024-06-18 14:40:46 +0530

    Extended Range and fixed bugs

--- booth.java
+++ booth.java
@@ -1,24 +1,37 @@
 import java.util.*;
 
 public class booth {
-//for digits ranging -15 to 15
+    static int n;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         char Qn, Qn1, tempCh = '0', sign;
-
-        String strAC = "00000"; // AC
 
         System.out.print("Multiplicand = "); // inputs
         int BR = input.nextInt();
         System.out.print("Multiplier = ");
         int QR = input.nextInt();
 
-        StringBuffer strbufQR = new StringBuffer(Integer.toBinaryString(Math.abs(QR)));
-        StringBuffer strbufBR = new StringBuffer(Integer.toBinaryString(Math.abs(BR)));
+        String binQR = Integer.toBinaryString(Math.abs(QR));
+        String binBR = Integer.toBinaryString(Math.abs(BR));
+
+        n = binBR.length() > binQR.length() ? binBR.length() : binQR.length();
+
+        int upperBound = (int) (Math.pow(2, n - 1) - 1);
+        int lowerBound = (int) -(Math.pow(2, n - 1) - 1);
+
+        if (BR < lowerBound || BR >= upperBound ||
+                QR < lowerBound || QR > upperBound)
+            n++;
+
+        StringBuffer strbufQR = new StringBuffer(binQR);
+        StringBuffer strbufBR = new StringBuffer(binBR);
+        StringBuffer strbufAC = new StringBuffer("");
 
         String strQR = setBinaryNum(strbufQR); // user-defined function
         String strBR = setBinaryNum(strbufBR); // user-defined function
+        String strAC = setBinaryNum(strbufAC); // user-defined function
 
         // if negative
         if (QR < 0)
@@ -31,20 +44,22 @@ public class booth {
         String ACQR = strAC.concat(strQR); // works right
         StringBuffer strbufACQR = new StringBuffer(strAC.concat(strQR));
 
-        System.out.println("BR: " + strBR);  //output prep.
+        System.out.println("BR: " + strBR); // output prep.
         System.out.println("QR: " + strQR);
+        System.out.println("BR': " + strCompBR);
+        System.out.println("n: " + n);
         System.out.println("  SC    AC      QR");
 
-        for (int SC = 5; SC > 0; SC--) {
+        for (int SC = n; SC > 0; SC--) {
             // logic of booth.
-            System.out.println("  " + SC + "   " + ACQR.substring(0, 5) + "   " + ACQR.substring(5));
+            System.out.println("  " + SC + "   " + ACQR.substring(0, n) + "   " + ACQR.substring(n));
 
-            Qn = ACQR.charAt(9);
+            Qn = ACQR.charAt(2 * n - 1);
             Qn1 = tempCh;
 
             if (Qn == '0' && Qn1 == '1') {
-                strAC = (strbufACQR.substring(0, 5)).toString();
-                strQR = (strbufACQR.substring(5)).toString();
+                strAC = (strbufACQR.substring(0, n)).toString();
+                strQR = (strbufACQR.substring(n)).toString();
 
                 strAC = binaryAdd(strAC, strBR);
 
@@ -52,8 +67,8 @@ public class booth {
             }
 
             else if (Qn == '1' && Qn1 == '0') {
-                strAC = (strbufACQR.substring(0, 5)).toString();
-                strQR = (strbufACQR.substring(5)).toString();
+                strAC = (strbufACQR.substring(0, n)).toString();
+                strQR = (strbufACQR.substring(n)).toString();
 
                 strAC = binaryAdd(strAC, strCompBR);
 
@@ -72,16 +87,16 @@ public class booth {
 
         }
 
-        System.out.println("  " + "0" + "   " + ACQR.substring(0, 5) + "   " + ACQR.substring(5));
+        System.out.println("  " + "0" + "   " + ACQR.substring(0, n) + "   " + ACQR.substring(n));
         System.out.println("Result: " + ACQR);
-        System.out.println("Decimal: " + binarytoDecimal(ACQR));
+        System.out.println("Decimal: " + (int) binarytoDecimal(ACQR));
 
         input.close();
     }
 
     static String setBinaryNum(StringBuffer strbuf) {
 
-        for (; strbuf.length() < 5;) {
+        for (; strbuf.length() < n;) {
             strbuf.insert(0, "0");
         }
 
@@ -92,17 +107,16 @@ public class booth {
     static String ashr(String str) {
         StringBuffer strbuf = new StringBuffer(str);
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < n - 1; i++) {
             strbuf.append(strbuf.charAt(0));
             strbuf.deleteCharAt(0);
         }
-
         return strbuf.toString();
     }
 
     static StringBuffer ashr(StringBuffer strbuf) {
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 2 * n - 1; i++) {
             strbuf.append(strbuf.charAt(0));
             strbuf.deleteCharAt(0);
         }
@@ -114,7 +128,7 @@ public class booth {
         int c, carry = 0;
         StringBuffer ans = new StringBuffer(""), strbufA = new StringBuffer(A), strbufB = new StringBuffer(B);
 
-        for (int i = 5; i > 0; i--) {
+        for (int i = n; i > 0; i--) {
 
             StringBuffer tempA = new StringBuffer(strbufA.substring(i - 1)); // one digit at a time in sequence from A
             tempA.setLength(1);
@@ -137,22 +151,22 @@ public class booth {
                 carry = 1;
                 ans.insert(0, 1);
             }
-            
+
         }
         return ans.toString();
     }
 
     static String twoscomplement(String A) {
         StringBuffer ans = new StringBuffer("");
-        int flag = 0;
+        boolean flag = true;
 
-        for (int i = 5; i > 0; i--) {
+        for (int i = n; i > 0; i--) {
 
             StringBuffer tempA = new StringBuffer(A.substring(i - 1));
             tempA.setLength(1);
             int digit = Integer.parseInt(tempA.toString());
 
-            if (flag == 0) {
+            if (flag) {
                 ans.insert(0, tempA.toString());
             }
 
@@ -163,26 +177,27 @@ public class booth {
                     ans.insert(0, "0");
             }
 
-            if (digit == 1)
-                flag++;
+            if (flag) {
+                if (digit == 1)
+                    flag = !flag;
+            }
 
         }
         return ans.toString();
     }
 
-    static double binarytoDecimal (String A){
-        double radix=0,ans=0;
-       
+    static double binarytoDecimal(String A) {
+        double radix = 0, ans = 0;
 
-        for (int i = 10; i > 0; i--) {
+        for (int i = 2 * n; i > 0; i--) {
 
             StringBuffer tempA = new StringBuffer(A.substring(i - 1)); // one digit at a time in sequence from A
             tempA.setLength(1);
-            
-            ans = ans + (Math.pow(2,radix)*Integer.parseInt(tempA.toString()));
-            radix++;
-    }
-    return ans;
 
-}
+            ans = ans + (Math.pow(2, radix) * Integer.parseInt(tempA.toString()));
+            radix++;
+        }
+        return ans;
+
+    }
 }
